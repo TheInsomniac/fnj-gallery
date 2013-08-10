dd714e973e96cf35ebc7b36a1dc80a0682a6ee7f
manxam <manxam2k@hotmail.com>
2013-08-09 18:11:11 -0600

    Quick refactor to global namespace vs not declaring variables. Fixed a , vs ; I missed.

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var fs = require("fs");
-config = JSON.parse(fs.readFileSync(__dirname + "/config.json"));
+global.config = JSON.parse(fs.readFileSync(__dirname + "/config.json"));
 
 var host = config.host,
     port = config.port;
 
 // create express app globally
-express = require("express"),
-app = new express();
+global.express = require("express");
+global.app = new express();
 
 // exposes "/"  and any files contained within static folder
 // used for css/ and js/ files.
 app.use(express.static(__dirname + "/static"));
 
 // get debug enabled/disabled from config
-debug = config.debug;
+global.debug = config.debug;
 // create tmp folder if !exists when debug enabled
 if (debug) {
   if (!fs.existsSync(__dirname + "/tmp")) {
@@ -22,19 +22,19 @@ if (debug) {
 }
 
 // create empty global variable to hold album data (we'll call this a cache)
-albums = null;
+global.albums = null;
 
 // load flickr module globally for use in routes
-flickr = require(__dirname + "/lib/flickr.js");
-USER_ID = config.user_id;
-OAUTH_TOKEN = config.oauth_token;
-OAUTH_SECRET = config.oauth_secret;
+global.flickr = require(__dirname + "/lib/flickr.js");
+global.USER_ID = config.user_id;
+global.OAUTH_TOKEN = config.oauth_token;
+global.OAUTH_SECRET = config.oauth_secret;
 
 // get collections or sets flag from config
-use = config.use;
+global.use = config.use;
 
 // load modules from "./routes" as defined in index.js 
-require('./routes');
+require("./routes");
 
 // determine if we want to run the http server. Loaded from config
 if (config.runServer) {
